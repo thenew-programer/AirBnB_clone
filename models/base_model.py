7dda155c8e65618d087ee76e4878fd092c9a7a5a
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-09 15:20:57 +0100

    BaseModel: task 3 completed

--- models/base_model.py
+++ models/base_model.py
@@ -17,42 +17,27 @@ class BaseModel:
                         everytime the object got changed
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime(
-            datetime.now().year,
-            datetime.now().month,
-            datetime.now().day,
-            datetime.now().hour,
-            datetime.now().minute,
-            datetime.now().second,
-        )
-        self.updated_at = datetime(
-            datetime.now().year,
-            datetime.now().month,
-            datetime.now().day,
-            datetime.now().hour,
-            datetime.now().minute,
-            datetime.now().second,
-        )
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """
         updates the public instance attribute updated_at with the current datetime
         """
-        self.updated_at = datetime(
-            datetime.now().year,
-            datetime.now().month,
-            datetime.now().day,
-            datetime.now().hour,
-            datetime.now().minute,
-            datetime.now().second,
-        )
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        _dict = self.__dict__.copy()
+        _dict["updated_at"] = self.updated_at.isoformat()
+        _dict["created_at"] = self.created_at.isoformat()
+        _dict["__class__"] = self.__class__.__name__
+        return _dict
 
     def __str__(self):
         """
         Overwrite the default behaviour of __str__ method
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-
-s = BaseModel()
-print(s)
