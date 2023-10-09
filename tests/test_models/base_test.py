3c1133b01ce8035bebf4e3f00991c1d80de5ed64
youssef bouryal <youssefbouryal02@gmail.com>
2023-10-09 16:34:56 +0100

    BaseModel Test: add entry point to test classes (#7)

--- tests/test_models/base_test.py
+++ tests/test_models/base_test.py
@@ -52,3 +52,7 @@ class TestBase_to_dict(unittest.TestCase):
         x = BaseModel()
         x.name = "BaseModel"
         self.assertNotEqual(x.to_dict(), x.__dict__())
+
+
+if __name__ == "__main__":
+    unittest.main()
