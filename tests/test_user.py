fb752654b039cdd67fbfa0f16f043bb9843f907d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-12 19:26:12 +0100

    Testing User: Set up user class for testing

---
+++ tests/test_user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Unittest for User class"""
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """TestUser class for testring User class"""
+
+    def setUp(self):
+        """Set up User for testing"""
+        self.user = User()
+        self.user.first_name = "youssef"
+        self.user.last_name = "bouryal"
+        self.user.email = "airbnb_clone@alx.com"
+        self.user.password = "1234password"
+
+
+
