73d766040b7c735daf03a5f014e63a59228600ef
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-13 07:24:09 +0100

    State: done

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Define State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class refer to the state of smt"""
+    name = ""
