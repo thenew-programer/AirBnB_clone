6aaa4d05cde9ac0cba5a8ec74a0950a86f6550a6
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-13 07:45:54 +0100

    Review: done

---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Define Review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review class
+
+    Public class attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
