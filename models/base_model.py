1a69c5ddceeb53bd78d956d7e907e417686dd4d3
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-10-15 20:07:02 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,43 @@
-#!/usr/bin/env python3
-"""BaseModel class"""
+#!/usr/bin/python3
+'''module base_model'''
 import uuid
 from datetime import datetime
-from dateutil import parser
-from models import storage
+import models
 
 
-class BaseModel:
-    """defines all common attributes/methods for other classes"""
-
+class BaseModel():
+    '''class BaseModel'''
     def __init__(self, *args, **kwargs):
-        """
-        BaseModel constructor
-        Args:
-            id: random uuid from uuid module
-            created_at: the date the object was created
-            updated_at: the date the object was updated it will change
-                        everytime the object got changed
-        """
-        if not kwargs:
+        '''class constructor for class BaseModel'''
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-
-                if key in ("created_at", "updated_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-
-                setattr(self, key, value)
-
-    def save(self):
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """
-        returns a dictionary containing all
-        keys/values of __dict__ of the instance
-        """
-        _dict = self.__dict__.copy()
-        _dict["id"] = str(self.id)
-        _dict["updated_at"] = self.updated_at.isoformat()
-        _dict["created_at"] = self.created_at.isoformat()
-        _dict["__class__"] = self.__class__.__name__
-        return _dict
+            models.storage.new(self)
 
     def __str__(self):
-        """
-        Overwrite the default behaviour of __str__ method
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        '''string of BaseModel instance'''
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        '''updates 'updated_at' instance with current datetime'''
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        '''dictionary representation of instance'''
+        _dict = dict(self.__dict__)
+        _dict['__class__'] = self.__class__.__name__
+        _dict['created_at'] = self.__dict__['created_at'].isoformat()
+        _dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        return _dict
